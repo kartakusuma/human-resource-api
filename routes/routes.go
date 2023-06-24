a71c57b6d17e735e74578189dcd2a6a83e4da659
Karta Kusuma <kartakusuma1199@gmail.com>
2023-06-25 00:05:21 +0700

    Create department routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,32 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"human-resource-api/config"
+	"human-resource-api/controllers"
+	"human-resource-api/models"
+	"human-resource-api/repository"
+	"human-resource-api/services"
+
+	"github.com/gin-gonic/gin"
+)
 
 func RouterSetup() *gin.Engine {
+	db := config.Connection()
+	models.MigrateModels(db)
+
 	router := gin.Default()
-
 	apiRoute := router.Group("/api")
-
 	apiV1Route := apiRoute.Group("/v1")
-	apiV1Route.GET("/", func(ctx *gin.Context) {})
+
+	departmentRepository := repository.NewDepartmentRepository(db)
+	departmentService := services.NewDepartmentService(departmentRepository)
+	departmentController := controllers.NewDepartmentController(departmentService)
+
+	apiV1Route.GET("/departments", departmentController.GetAll)
+	apiV1Route.POST("/departments", departmentController.Store)
+	apiV1Route.GET("/departments/:id", departmentController.GetByID)
+	apiV1Route.PUT("/departments/:id", departmentController.Update)
+	apiV1Route.DELETE("/departments/:id", departmentController.Destroy)
 
 	return router
 }
