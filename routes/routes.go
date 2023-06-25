df4b6f1130e15feb88e84d1fb3d42c8037ce3b8a
Karta Kusuma <kartakusuma1199@gmail.com>
2023-06-25 15:25:09 +0700

    Add employee routes

--- routes/routes.go
+++ routes/routes.go
@@ -18,6 +18,7 @@ func RouterSetup() *gin.Engine {
 	apiRoute := router.Group("/api")
 	apiV1Route := apiRoute.Group("/v1")
 
+	// department routes
 	departmentRepository := repository.NewDepartmentRepository(db)
 	departmentService := services.NewDepartmentService(departmentRepository)
 	departmentController := controllers.NewDepartmentController(departmentService)
@@ -27,6 +28,17 @@ func RouterSetup() *gin.Engine {
 	apiV1Route.GET("/departments/:id", departmentController.GetByID)
 	apiV1Route.PUT("/departments/:id", departmentController.Update)
 	apiV1Route.DELETE("/departments/:id", departmentController.Destroy)
+
+	// employee routes
+	employeeRepository := repository.NewEmployeeRepository(db)
+	employeeService := services.NewEmployeeService(employeeRepository)
+	employeeController := controllers.NewEmployeeController(employeeService)
+
+	apiV1Route.GET("/employees", employeeController.GetAll)
+	apiV1Route.POST("/employees", employeeController.Store)
+	apiV1Route.GET("/employees/:id", employeeController.GetByID)
+	apiV1Route.PUT("/employees/:id", employeeController.Update)
+	apiV1Route.DELETE("/employees/:id", employeeController.Destroy)
 
 	return router
 }
